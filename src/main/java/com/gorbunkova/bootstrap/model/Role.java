15c8e2b3bb5dba4d4f211bffda0d1aa7ca4b162e
Gorbunkovaya <dgorbunkovaya@gmail.com>
2021-08-31 01:40:10 +0300

    Initial commit

--- src/main/java/com/gorbunkova/bootstrap/model/Role.java
+++ src/main/java/com/gorbunkova/bootstrap/model/Role.java
@@ -56,7 +56,7 @@ public class Role implements GrantedAuthority {
 
     @Override
     public String toString() {
-        return role;
+        return role.replace("ROLE_", "");
     }
 
     @Override
